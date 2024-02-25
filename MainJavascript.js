fbb489b5bb09495d17117ad3114ebbff8883ba0f
Jacob385 <122482836+Jacob385@users.noreply.github.com>
2024-02-24 22:54:33 -0600

    Added MainJavascript.js

---
+++ MainJavascript.js
@@ -0,0 +1,5 @@
+//Go to Home page
+function goToHome(){window.location.href ="https://jacob385.github.io"}
+
+//Go to test page
+function goToTest(){window.location.href ="https://jacob385.github.io/test"}
