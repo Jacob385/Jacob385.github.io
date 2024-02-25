815c7e606685b6abfc92f1eb18513b27f73ea7a7
Jacob385 <122482836+Jacob385@users.noreply.github.com>
2024-02-25 00:03:00 -0600

    github size

--- MainJavascript.js
+++ MainJavascript.js
@@ -3,3 +3,6 @@ function goToHome(){window.location.href ="https://jacob385.github.io"}
 
 //Go to test page
 function goToTest(){window.location.href ="https://jacob385.github.io/test"}
+
+//Go to Github
+function goToGithub(){window.location.href ="https://github.com/Jacob385"}
