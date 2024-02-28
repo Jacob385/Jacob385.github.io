8ef9e306f267e4860f96833391fa333a9ca56d05
Jacob385 <122482836+Jacob385@users.noreply.github.com>
2024-02-28 15:43:19 -0600

    Added tools page and intchar page

--- MainJavascript.js
+++ MainJavascript.js
@@ -1,8 +1,14 @@
 //Go to Home page
 function goToHome(){window.location.href ="https://jacob385.github.io"}
 
-//Go to test page
-function goToTest(){window.location.href ="https://jacob385.github.io/test"}
-
 //Go to Github
 function goToGithub(){window.location.href ="https://github.com/Jacob385"}
+
+//Go to tools page
+function goToTools(){window.location.href ="https://jacob385.github.io/tools"}
+
+//Go to reverse text page
+function goToReverse(){window.location.href ="https://jacob385.github.io/reverse"}
+
+//Go to int-char converter page
+function goToIntchar(){window.location.href ="https://jacob385.github.io/intchar"}
